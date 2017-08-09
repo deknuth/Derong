d4a06152bd78ee66021acf26d7d41ff756edacb6
lovenix <lovenix@live.cn>
2017-08-09 18:02:46 +0800

    20170809
    key ctrl code modify

--- key/keyV1.1-8/main.c
+++ key/keyV1.1-8/main.c
@@ -8,7 +8,6 @@
 #include <math.h>
 #include <util/delay.h>
 #include <util/twi.h>
-#include <avr/iom48p.h>
 
 #define LOW           0
 #define HIGH          (!LOW)
@@ -18,46 +17,32 @@
 #define setbit(x,y) x |= (1<<y)
 #define clrbit(x,y) x &= ~(1<<y)
 
-#define SPI_SCS_H	PORTB|=1		// PB0
-#define	SPI_SCS_L	PORTB&=~1
-
-#define	SPI_SCK_H	PORTB|=1<<1
-#define	SPI_SCK_L	PORTB&=~(1<<1)
-
-#define	SPI_DATA_OUT	DDRB|=1<<3
-#define	SPI_DATA_IN   	DDRB&=~(1<<3)
-#define	SPI_DATA_H		PORTB|=1<<3
-#define	SPI_DATA_L		PORTB&=~(1<<3)
-#define	SPI_DATA_HL		PINB&(1<<3)
-
-#define	LED_ON		PORTB|=(1<<1)
-#define	LED_OFF		PORTB&=~(1<<1)
-#define	LED_BLINK	PORTB^=(1<<1)
-
 #define BAUD	38400
 
 #define T1_OFF { TCCR1B=0; TIMSK&=~(1<<TOIE1); TCNT1=0; }
 #define T1_ON { TCCR1B|=1<<CS11; TIMSK|=1<<TOIE1; TCNT1=0; }	// 8 divide
 #define R_LEN	16
 
-#define T2_ON	{ TCCR2=1<<CS22|1<<CS20; TIMSK|=1<<TOIE2; TCNT2=0;}	// 128 divide  2.1ms
-#define T2_OFF	{ TCCR2=0; TIMSK&=~1<<TOIE2; }
+#define T2_ON	{ TCCR2B=1<<CS22|1<<CS20; TIMSK2|=1<<TOIE2; TCNT2=0;}	// 128 divide  2.1ms
+#define T2_OFF	{ TCCR2B=0; TIMSK2&=~1<<TOIE2; }
 
 
-#define RS_595		{ PORTA&=~1; _delay_us(5); PORTA|=1; }	
-#define SCK_595_H	PORTA|=(1<<1)
-#define SCK_595_L	PORTA&=~(1<<1)
-#define RCK_595_H	PORTA|=(1<<2)
-#define RCK_595_L	PORTA&=~(1<<2)
-#define DATA_595_H	PORTA|=(1<<3)
-#define DATA_595_L	PORTA&=~(1<<3)
+#define RS_595		{ PORTC&=~8; _delay_us(5); PORTC|=8; }	
+#define SCK_595_H	PORTC|=4
+#define SCK_595_L	PORTC&=~4
+#define RCK_595_H	PORTC|=2
+#define RCK_595_L	PORTC&=~2
+#define DATA_595_H	PORTC|=1			// PC0
+#define DATA_595_L	PORTC&=~1
+#define EN_4051		PORTC&=~0x10
+#define DIS_4051	PORTC|=0x10
 
 #define TX_ENA	PORTD|=0x04
 #define RX_ENA	PORTD&=~0x04
 #define MAX_R_LEN 16
 
-#define BEEP_100MS	{ PORTC|=1<<5; _delay_ms(150); PORTC&=~1<<5; }
-#define FEED_DOG	PORTB^=1<<4;
+#define BEEP_100MS	{ PORTB|=0x20; _delay_ms(150); PORTB&=~0x20; }
+#define FEED_DOG	PORTC^=0x20;
 //#define TEST
 
 volatile unsigned char  U0Ready = 0;
@@ -69,27 +54,25 @@ unsigned char cBuf[6] = { 0xFC,0x00,0x00,0x00,0x00,0xFF };
 void PortInit(void)
 {
     DDRB = 0B00110111;		// PB4->595_OE PB5->beep  PB1->IN1A PB2->IN1B PB3->IN1C
-    PORTB= 0B00000000;		// PB2->595 en
+    PORTB= 0B00000000;		// PB2->595_EN
     PINB = 0x00;
 
     DDRC = 0B00111111;		// PC5->dog PC4->4051_EN
-    PORTC= 0B00011111;
+    PORTC= 0B00001111;
     PINC = 0x00;
 
-    DDRD = 0B00000100;		// PD2->485-DR
+    DDRD = 0B00000100;		// PD2->485-DR  PD3->SIG
     PORTD= 0B00000000;
     PIND = 0x00;
-
 }
 
 void UartInit(void)
 {
-    UBRRH = (F_CPU / BAUD / 16 - 1) / 256;
-    UBRRL = (F_CPU / BAUD / 16 - 1) % 256;
-    UCSRB = 1<<RXEN | 1<<TXEN | 1<<RXCIE;
-    UCSRC = 1<<UCSZ0 | 1<<UCSZ1 | 1<<URSEL;
+    UBRR0H = (F_CPU / BAUD / 16 - 1) / 256;
+    UBRR0L = (F_CPU / BAUD / 16 - 1) % 256;
+    UCSR0B = 1<<RXEN0 | 1<<TXEN0 | 1<<RXCIE0;
+    UCSR0C = 1<<UCSZ00 | 1<<UCSZ01;
 }
-
 
 void R485Send(unsigned char* data,unsigned char len)
 {
@@ -98,8 +81,8 @@ void R485Send(unsigned char* data,unsigned char len)
     _delay_us(20);
     for(i=0; i<len; i++)
     {
-        while(!(UCSRA & (1 << UDRE)));
-        UDR = *(data++);
+        while(!(UCSR0A & (1 << UDRE0)));
+        UDR0 = *(data++);
     }
  //   RX_ENA;
 }
@@ -117,27 +100,22 @@ unsigned char dis[4] = {0};
 volatile unsigned char count = 0;
 unsigned char tmp;
 
-ISR(INT1_vect)
-{
-	
-}
 
-ISR(SIG_OVERFLOW2)			// UART0 timeout
+ISR(TIMER2_OVF_vect)			// UART0 timeout
 {
     U0Ready = 1;
     T2_OFF;
 }
 
-ISR(USART_RXC_vect)
+ISR(USART_RX_vect)
 {
     unsigned char tmp;
     T2_ON;
-    tmp = UDR;
+    tmp = UDR0;
     rBuf[U0Count++] = tmp;
     if(U0Count >= MAX_R_LEN)
         U0Count = 0;
 }
-
 
 void Send595(unsigned long data)
 {
@@ -159,32 +137,28 @@ void Send595(unsigned long data)
 }
 
 const unsigned long bit[26] = {
-    0x01,0x80,0x40,0x20,0x10,0x8,0x4,0x02,
+    0x80,0x40,0x20,0x10,0x8,0x4,0x02,0x01,
     0x8000,0x4000,0x2000,0x1000,0x800,0x400,0x200,0x100,
     0x400000,0x800000,0x200000,0x100000,0x20000,0x40000,0x80000,0x10000
 };
-unsigned char key_stat[26] = {0};
-unsigned char scan[5] = {0};
-unsigned long key_stat_l = 0;
+unsigned char key_stat[8] = {0};
+
 void KeyScan(void)
 {
-    unsigned char i,j;
-    unsigned char temp;
-    for(i=0; i<5; i++) //(ROW1->ROW5 <=> PD3->PD7)  (COL1->COL5 <=> PC0>PC4)
-    {
-        PORTC &= ~(1<<i);
-        _delay_ms(5);
-        temp = ((PIND&0xF8)>>3);
-        _delay_ms(5);
-        scan[i] = temp;
-        PORTC |= (1<<i);
+    unsigned char i;
+    EN_4051;
+    PORTB &= 0xF8;
+    for(i=0; i<8; i++) 
+    {	
+		PORTB |= i;
+		_delay_us(1);
+		if(PIND&0x08)
+			key_stat[i] = 1;
+		else
+			key_stat[i] = 0;
+		PORTB &= ~i;
     }
-
-	for(i=0; i<5; i++)	// scan over
-    {
-        for(j=0; j<5; j++)
-            key_stat[i*5+j] = ((scan[i]>>j)&0x01);
-    }
+  	DIS_4051;
 }
 
 int main(void)
@@ -197,34 +171,26 @@ int main(void)
 	RS_595;
     Send595(0);
     UartInit();
-    //    T0Init();
     sei();
 	
-	PORTC |= 1<<5;
-    _delay_ms(150);
-	PORTC &= ~1<<5;
-	_delay_ms(150);
-	PORTC |= 1<<5;
-    _delay_ms(150);
-	PORTC &= ~1<<5;
-	_delay_ms(150);
-	PORTC |= 1<<5;
-    _delay_ms(150);
-	PORTC &= ~1<<5;
-	_delay_ms(150);
+	for(i=0; i<3; i++)	
+	{
+		BEEP_100MS;
+		_delay_ms(150);
+	}
 	
-	encode = (PINA>>7)+((PINA&0x40)>>5)+((PINA&0x20)>>3)+((PINA&0x10)>>1);
+	encode = ((PIND&0xF0)>>4);		// ВІТы
 	
 #ifdef TEST
-	unsigned char i;
+
 	while(1)
 	{
-		for(i=0;i<16;i++)
+		for(i=0;i<8;i++)
 		{
 			Send595(bit[i]);
-			BEEP_100MS;
+	//		BEEP_100MS;
+			_delay_ms(300);
 			Send595(0);
-			_delay_ms(100);
 			FEED_DOG;
 		}
 	}
@@ -245,7 +211,7 @@ int main(void)
 						case 1:			// control
 						{
 							keyBit = rBuf[3];
-							if(keyBit < 24)
+							if(keyBit < 8)
 							{
 								Send595(bit[keyBit]);
 								BEEP_100MS;
@@ -263,10 +229,8 @@ int main(void)
 						{
 							cBuf[1] = rBuf[1];		// key board code
 							memset(&cBuf[2],0x00,3);
-							
-							for(i=0;i<24;i++)
+							for(i=0;i<8;i++)
 								cBuf[4-i/8] |= (key_stat[i]<<(i%8));
-								
 							R485Send(cBuf,6);
 							break;
 						}
